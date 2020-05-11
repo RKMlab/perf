dabde42284aa7c13313a9bb587fa1d449a4c6ffd
Avvaru Akshay <avvaru@csirccmb.org>
2020-05-11 17:42:40 +0530

    version error

--- PERF/core.py
+++ PERF/core.py
@@ -24,7 +24,7 @@ def getArgs():
     """
     Parses command line arguments and returns them to the caller
     """
-    __version__ = 'v0.4.1'
+    __version__ = 'v0.4.4'
     parser = argparse.ArgumentParser()
     parser._action_groups.pop()
 
